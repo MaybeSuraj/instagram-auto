27f5b84c80588f2a673f0465ac168373ef0c9532
Maybesuraj <gamingchiill15@gmail.com>
2026-06-26 13:59:20 +0530

    Implement HTTP server with basic request handling and add start script

--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,31 @@ import uvloop
 from pyrogram import idle
 from logger import LOGGER
 from app import bot, botname, loop
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Only respond if the request path is exactly "/"
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello World\n")
+        else:
+            # Send a 404 error for any other URL path
+            self.send_error(404, "Page Not Found")
+
+    def log_message(self, format, *args):
+        pass  # disables default HTTP request logging
+
+
+def run():
+    server_address = ("0.0.0.0", 10000)
+    httpd = HTTPServer(server_address, MyHandler)
+    print("Server running on port 10000...")
+    httpd.serve_forever()
 
 
 uvloop.install()
@@ -15,5 +40,10 @@ async def initiate_bot():
 
 
 if __name__ == "__main__":
+    # Start the HTTP server in a separate thread
+    server_thread = Thread(target=run)
+    server_thread.daemon = True  # This makes sure the thread will exit when the main
+    # program exits
+    server_thread.start()
     loop.run_until_complete(initiate_bot())
     LOGGER(__name__).info(f"{botname} IS STOPPED")
